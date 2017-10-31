2a1a10657f68dad4ca7905218c8ca49089222855
Alex Bogomolov <alexbogomolov.kharkiv@gmail.com>
2017-10-31 18:40:40 +0200

    add content provider

---
+++ app/src/main/java/com/bogomolov/alexander/androidlab/NotesProvider.java
@@ -0,0 +1,57 @@
+package com.bogomolov.alexander.androidlab;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by admin on 31.10.2017.
+ */
+
+public class NotesProvider extends ContentProvider {
+    static final String AUTHORITY = "com.bogomolov.alexander.androidlab.notes";
+
+    private NotesDbHelper dbHelper;
+
+    @Override
+    public boolean onCreate() {
+        this.dbHelper = new NotesDbHelper(getContext());
+        return true;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        Cursor cursor = db.query(NotesContract.NotesEntry.TABLE_NAME, projection, selection, selectionArgs, null, null, sortOrder);
+
+
+        return cursor;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
